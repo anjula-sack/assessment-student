22e87c47e3f772e30e5b534499145790689f8933
anjula-sack <anjulashanaka@gmail.com>
2025-12-01 10:08:03 +0530

    Fix school name issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -312,6 +312,7 @@ function PageContent() {
                   setFormData((prev) => ({
                     ...prev,
                     school: e.target.value,
+                    isNewSchool: false,
                   }))
                 }
                 className="w-full px-3 py-2 border border-gray-300 rounded-md 
